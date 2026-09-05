14f8ddc77860fdfef441050f2d3ef5f8796d3433
Ayush Rai <raiayushrai2004@gmail.com>
2026-09-05 21:07:33 +0530

    optimize entire project

--- backend/index.js
+++ backend/index.js
@@ -218,46 +218,6 @@ const getAccountSummary = async (userId) => {
 //   res.send("holdings data saved!");
 // });
 
-// app.get("/addPositions", (req, res) => {
-//   const tempPositions = [
-//     {
-//       product: "CNC",
-//       name: "EVEREADY",
-//       qty: 2,
-//       avg: 316.27,
-//       price: 312.35,
-//       net: "+0.58%",
-//       day: "-1.24%",
-//       isLoss: true,
-//     },
-//     {
-//       product: "CNC",
-//       name: "JUBLFOOD",
-//       qty: 1,
-//       avg: 3124.75,
-//       price: 3082.65,
-//       net: "+10.04%",
-//       day: "-1.35%",
-//       isLoss: true,
-//     },
-//   ];
-
-//   tempPositions.forEach((position) => {
-//     let newPosition = new PositionsModel({
-//       product: position.product,
-//       name: position.name,
-//       qty: position.qty,
-//       avg: position.avg,
-//       price: position.price,
-//       net: position.net,
-//       day: position.day,
-//       isLoss: position.isLoss,
-//     });
-//     newPosition.save();
-//   });
-//   res.send("postions data saved");
-// });
-
 app.get("/allHoldings", authMiddleware, async (req, res) => {
   let allHoldings = await HoldingsModel.find({ userId: req.user.id });
   res.json(allHoldings);
--- backend/model/PositionsModel.js
+++
@@ -1,6 +0,0 @@
-import { model } from "mongoose";
-import PositionsSchema from "../schemas/PositionsSchema.js";
-
-const PostionsModel = new model("Postions", PositionsSchema);
-
-export default PostionsModel;
